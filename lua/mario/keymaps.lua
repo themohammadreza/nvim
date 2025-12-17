5c06e876eab378629bb5b5e90bb869c53b280ace
Mohammadreza Naseri <161240288+themohammadreza@users.noreply.github.com>
2025-12-17 09:29:39 +0330

    Delete lua/mario directory

--- lua/mario/keymaps.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = "\\"
-
-
-vim.keymap.set('n', '<leader>r', ':w<CR>:terminal cargo run<CR>')
-vim.keymap.set('n', '<Tab>', ':bn<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
-vim.keymap.set('n', '<F5>', ':!g++ *.cpp -o main && ./main<CR>')
